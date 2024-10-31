83409dbccacaf23a8fb55bfca2f4d6fae5ca41d0
edsincp <edsonpereiracarvalho10@gmail.com>
2024-10-31 13:43:17 -0300

    Curso em andamento

---
+++ projeto2/src/Titulo.jsx
@@ -0,0 +1,13 @@
+function Titulo(){
+  let nome = "Edson Pereira de Carvalho";
+  const soma = 10 + 10; 
+
+  return (
+    <div>
+      <h1>Eu sou {nome}</h1>
+      <p>Eu sou fo@a</p>
+    </div>
+  ) 
+}
+
+export default Titulo
